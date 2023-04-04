288d210434db62255db358f7095e84bf11b516aa
ciceu.gabi <ciceu.gabi@gmail.com>
2023-04-04 10:52:11 +0300

    Initial commit

---
+++ src/test/java/Gamification/GamificationApplicationTests.java
@@ -0,0 +1,13 @@
+package Gamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
